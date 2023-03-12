82d23f5154700426e790bcba5cc2fbe2e8d827ae
David Olagookun <erijesudo@gmail.com>
2023-03-12 16:39:22 +0100

    Initial Commit

---
+++ Expense Item/Expense_ItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_ItemApp.swift
+//  Expense Item
+//
+//  Created by David OH on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Expense_ItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
